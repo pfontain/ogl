119c43a46113d0185d9395ea5467493a93981f6e
Paulo Gomes <pauloxgomes@gmail.com>
2018-08-19 11:24:37 -0400

    using vertex array objects

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -9,6 +9,8 @@ GLFWwindow* window;
 #include <glm/glm.hpp>
 using namespace glm;
 
+#include "shader.hpp"
+
 int main( void )
 {
 	// Initialise GLFW
@@ -37,6 +39,7 @@ int main( void )
 	glfwMakeContextCurrent(window);
 
 	// Initialize GLEW
+    glewExperimental = true; // Needed for core profile
 	if (glewInit() != GLEW_OK) {
 		fprintf(stderr, "Failed to initialize GLEW\n");
 		getchar();
@@ -46,15 +49,65 @@ int main( void )
 
 	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-
-	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    
+    GLuint vertexArrayID;
+    
+    // Generate a name for a new array.
+    glGenVertexArrays(1, &vertexArrayID);
+    
+    // Make the new array active, creating it if necessary.
+    glBindVertexArray(vertexArrayID);
+    
+    // Create and compile our GLSL program from the shaders
+    GLuint programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader" );
+    
+    // An array of 3 vectors which represents 3 vertices
+    static const GLfloat g_vertex_buffer_data[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f,  1.0f, 0.0f,
+    };
+    
+    // This will identify our vertex buffer
+    GLuint vertexbuffer;
+    
+    // Generate 1 buffer, put the resulting identifier in vertexbuffer
+    glGenBuffers(1, &vertexbuffer);
+    
+    // The following commands will talk about our 'vertexbuffer' buffer
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    
+    // Give our vertices to OpenGL.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+    
+    // Attach the active buffer to the active array,
+    // as an array of vectors with 3 floats each.
+    GLint vertexLoc = glGetAttribLocation(programID, "InVertex");
+    glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    
+    // Enable the vertex attribute
+    glEnableVertexAttribArray(vertexLoc);
+    
+    // Make sure the VAO is not changed from outside code
+    glBindVertexArray(0);
+    
+    // Dark blue background
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
 	do{
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
+        glUseProgram(programID);
+        
+        glBindVertexArray(vertexArrayID);
 
-		// Draw nothing, see you in tutorial 2 !
+        // 1st attribute buffer : vertices
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        // Draw the triangle !
+        glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
 
+        glBindVertexArray(0);
+        
 		// Swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
